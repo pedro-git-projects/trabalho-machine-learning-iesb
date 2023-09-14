599da93fda54f33543731807378ae14a4346570b
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-09-13 23:55:49 -0300

    adding docstrings|updating makefile

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+run:
+	python src/main.py
+
 test:
 	pytest src/tests/test_csv_processor.py > log.txt
 
--- src/processors/csv_processor.py
+++ src/processors/csv_processor.py
@@ -3,7 +3,8 @@ import statistics
 import matplotlib.pyplot as plt
 
 
-"""
+def processar_csv(input_file: str, output_file: str) -> None:
+    """
     Processa um arquivo CSV de entrada, tratando valores inválidos nas colunas 'age', calculando a moda das idades
     válidas e substituindo os valores inválidos pela moda. O resultado é salvo em um novo arquivo CSV.
 
@@ -20,22 +21,13 @@ import matplotlib.pyplot as plt
         >>> processar_csv('dados.csv', 'dados_processados.csv')
     """
 
-
-def processar_csv(input_file: str, output_file: str) -> None:
     try:
-        # lê o arquivo
         df = pd.read_csv(input_file)
 
-        # remove valores inválidos
         valid_ages = df["age"].dropna()
-
-        # calcula a moda das idades
         mode_ages = statistics.mode(valid_ages)
-
-        # substitui as idades inválidas pela moda
         df["age"].fillna(mode_ages, inplace=True)
 
-        # salva em um arquivo
         df.to_csv(output_file, index=False)
 
     except FileNotFoundError:
@@ -45,13 +37,30 @@ def processar_csv(input_file: str, output_file: str) -> None:
 
 
 def plot_porcentagem_sobreviventes(csv_file: str, output_file: str) -> None:
+    """
+    Gera um gráfico de pizza representando a porcentagem de sobreviventes e não sobreviventes
+    com base nos dados de um arquivo CSV e salva o gráfico em um arquivo de saída.
+
+    Args:
+        csv_file (str): O caminho para o arquivo CSV contendo os dados.
+        output_file (str): O caminho para o arquivo de saída onde o gráfico será salvo.
+
+    Returns:
+        None
+
+    Raises:
+        FileNotFoundError: Se o arquivo CSV especificado não for encontrado.
+        pd.errors.ParserError: Se o arquivo CSV não estiver em formato válido.
+
+    Example:
+        plot_porcentagem_sobreviventes('dados.csv', 'grafico_pizza.png')
+    """
+
     try:
         df = pd.read_csv(csv_file)
 
-        # contagem de sobreviventes e não sobreviventes
         sobreviventes = df["survived"].value_counts()
 
-        # plotar o gráfico de pizza
         plt.figure(figsize=(6, 6))
         plt.pie(
             sobreviventes,
@@ -69,6 +78,26 @@ def plot_porcentagem_sobreviventes(csv_file: str, output_file: str) -> None:
 
 
 def scatter_age_vs_fare(csv_file: str, save_path=None) -> None:
+    """
+    Cria um gráfico de dispersão para visualizar a relação entre a idade e a tarifa
+    com base nos dados de um arquivo CSV. O gráfico pode ser salvo em um arquivo ou exibido na tela.
+
+    Args:
+        csv_file (str): O caminho para o arquivo CSV contendo os dados.
+        save_path (str, optional): O caminho para salvar o gráfico. Se None, o gráfico será exibido na tela.
+                                   (default is None)
+
+    Returns:
+        None
+
+    Raises:
+        FileNotFoundError: Se o arquivo CSV especificado não for encontrado.
+        pd.errors.ParserError: Se o arquivo CSV não estiver em formato válido.
+
+    Example:
+        scatter_age_vs_fare('dados.csv', save_path='grafico_dispersao.png')
+    """
+
     try:
         df = pd.read_csv(csv_file)
 
@@ -93,15 +122,35 @@ def scatter_age_vs_fare(csv_file: str, save_path=None) -> None:
 
 
 def calcular_somatorio_genero(csv_file: str) -> None:
+    """
+    Calcula o somatório de homens e mulheres com base nos dados de um arquivo CSV
+    e apresenta o resultado no terminal.
+
+    Args:
+        csv_file (str): O caminho para o arquivo CSV contendo os dados.
+
+    Returns:
+        None
+
+    Raises:
+        FileNotFoundError: Se o arquivo CSV especificado não for encontrado.
+        pd.errors.ParserError: Se o arquivo CSV não estiver em formato válido.
+
+    Example:
+        calcular_somatorio_genero('dados.csv')
+    """
     try:
         df = pd.read_csv(csv_file)
 
-        # calcula o somatório de homens e mulheres
         somatorio_genero = df["sex"].value_counts()
+        total_homens = somatorio_genero.get("male", 0)
+        total_mulheres = somatorio_genero.get("female", 0)
 
-        # apresenta o somatório no terminal
-        print("Somatório de Homens e Mulheres:")
-        print(somatorio_genero)
+        total = total_homens + total_mulheres
+
+        print(f"Total de Homens: {total_homens}")
+        print(f"Total de Mulheres: {total_mulheres}")
+        print(f"Total Geral: {total}")
 
     except FileNotFoundError:
         print(f"O arquivo {csv_file} não foi encontrado.")
