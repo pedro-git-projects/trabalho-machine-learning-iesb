d7966a481838870404ac9c869a77a27ffd215ad4
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-09-28 10:58:23 -0300

    updating Make recipes|creating setup.py|updating pip requirements|unit testing processar_csv

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
+install:
+	python setup.py install
+
 run:
 	python src/main.py
 
 test:
-	pytest src/tests/test_csv_processor.py > log.txt
+	pytest src/tests/test_csv_processor.py 
 
+pydoc:
+	pydoc -w ./src/processors/csv_processor.py
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name="trabalho_ml_iesb",
+    version="1.0.0",
+    description="solução das tarefas relacionadas à análise do arquivo dados.csv",
+    author="Pedro Martins",
+    url="https://github.com/pedro-git-projects/trabalho-machine-learning-iesb",
+    install_requires=[
+        "contourpy==1.1.1",
+        "cycler==0.11.0",
+        "fonttools==4.42.1",
+        "iniconfig==2.0.0",
+        "kiwisolver==1.4.5",
+        "matplotlib==3.8.0",
+        "numpy==1.25.2",
+        "packaging==23.1",
+        "pandas==2.1.0",
+        "Pillow==10.0.1",
+        "pluggy==1.3.0",
+        "pyparsing==3.1.1",
+        "pytest==7.4.2",
+        "python-dateutil==2.8.2",
+        "pytz==2023.3.post1",
+        "six==1.16.0",
+        "trabalho-ml-iesb==1.0.0",
+        "tzdata==2023.3",
+    ],
+)
